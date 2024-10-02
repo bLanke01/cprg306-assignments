d0a7e10e225b32569550ddd2f8dea6004a53e07d
bLanke01 <troyescuadro1748@gmail.com>
2024-10-02 04:39:11 -0600

    finished

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href = "http://localhost:3000/week-2"> Week-2</Link>
+      <Link href = "http://localhost:3000/week-2"> Week-2</Link> <br />
+      <Link href = "http://localhost:3000/week-3"> Week-3</Link>
     </div>
   );
 }
