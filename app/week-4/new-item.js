68b80296083f6e582d39bbdf917342966f19c723
bLanke01 <troyescuadro1748@gmail.com>
2024-10-08 21:20:48 -0600

    finished week 4

---
+++ app/week-4/new-item.js
@@ -0,0 +1,18 @@
+"use client";
+
+import { useState } from "react";
+
+export default function NewItem() {
+    const [quantity, setQuantity] = useState(0);
+
+    const increment = () => setQuantity(quantity + 1 );
+    const decrement = () => setQuantity(quantity - 1 );
+
+    return (
+        <div className="max-w-md w-full mx-auto p-6 bg-gray-100 rounded-lg shadow-md overflow-hidden flex justify-center space-x-4">        
+            <p className="text-xl font-semibold text-black mt-4 text-center">Quantity: {quantity}</p>
+            <button onClick={increment} disabled={quantity >= 20} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Add</button>
+            <button onClick={decrement} disabled={quantity <= 0} className="bg-gray-300 hover:bg-gray-400 text-black font-semibold py-2 px-4 rounded">Delete</button>
+        </div>
+     );
+}
