4ec039fc2d8248bd446737a61f3ef6e655b3f329
bLanke01 <troyescuadro1748@gmail.com>
2024-10-23 06:00:30 -0600

    finished week-6

--- app/page.js
+++ app/page.js
@@ -7,7 +7,8 @@ export default function Home() {
       <Link href = "http://localhost:3000/week-2"> Week-2</Link> <br />
       <Link href = "http://localhost:3000/week-3"> Week-3</Link> <br />
       <Link href = "http://localhost:3000/week-4"> Week-4</Link> <br />
-      <Link href = "http://localhost:3000/week-5"> Week-5</Link> 
+      <Link href = "http://localhost:3000/week-5"> Week-5</Link> <br />
+      <Link href = "http://localhost:3000/week-6"> Week-6</Link>
     </div>
   );
 }
---
+++ app/week-6/item-list.js
@@ -0,0 +1,55 @@
+"use client";
+
+import Item from "./item.js";
+import { useState } from "react";
+import items from "./item.json";
+
+
+
+export default function ItemList() {
+
+  const [sortBy, setSortBy] = useState("name");
+
+  
+  let filteredItems = [...items];
+  filteredItems.sort((a, b) => {
+    if (sortBy === "name") {
+      return a.name.localeCompare(b.name);
+    } else {
+      return a.category.localeCompare(b.category);
+    }
+  });
+  
+    return (
+      
+      <section className="container mx-auto px-4 py-8">
+        <div className="flex justify-center w-full mb-8">
+        <button
+          onClick={() => setSortBy("name")}
+          className={`px-4 py-2 rounded ${
+            sortBy === "name" ? "bg-blue-500 text-white" : "bg-gray-200"
+          } mr-2`}
+        >Name </button>
+        <button
+          onClick={() => setSortBy("category")}
+          className={`px-4 py-2 rounded ${
+            sortBy === "category" ? "bg-green-500 text-white" : "bg-gray-200"
+          }ml-2`}
+        >Category </button>
+        </div>
+        <div className="mt-8">
+        <h2 className="text-xl font-bold mb-4">List of All Items</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {filteredItems.map((item) => (
+            <Item
+              key={item.id}
+              name={item.name}
+              quantity={item.quantity}
+              category={item.category}
+            />
+          ))}
+        </div>
+        </div>
+      </section>
+    );
+}
