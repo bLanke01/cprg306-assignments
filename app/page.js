236ee69b42d4597e3d57c922d589db4ea312b322
bLanke01 <troyescuadro1748@gmail.com>
2024-11-06 07:16:29 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,7 +9,9 @@ export default function Home() {
       <Link href = "http://localhost:3000/week-4"> Week-4</Link> <br />
       <Link href = "http://localhost:3000/week-5"> Week-5</Link> <br />
       <Link href = "http://localhost:3000/week-6"> Week-6</Link> <br />
-      <Link href = "http://localhost:3000/week-7"> Week-7</Link> 
+      <Link href = "http://localhost:3000/week-7"> Week-7</Link> <br />
+      <Link href = "http://localhost:3000/week-8"> Week-8</Link> <br />
+      <Link href = "http://localhost:3000/week-9"> Week-9</Link> <br />
     </div>
   );
 }
